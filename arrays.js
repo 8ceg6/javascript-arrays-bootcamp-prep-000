1a93cd5b984bcaf0bdeacb54a035817377e297c4
cedric e goddard <8ceg6@comcast.net>
2019-07-10 03:07:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function accessElementInArray(array, index){
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
           array.shift()
-}
+}       return array
 function destructivelyRemoveElementFromBeginningOfArray (array){
           return array.slice(1)
   
