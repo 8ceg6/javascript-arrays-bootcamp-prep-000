89ff9d8329e4d2db3759899277727d9758e3ea1d
cedric e goddard <8ceg6@comcast.net>
2019-07-10 02:59:17 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(chocolateBars, foo){
+          return ["foo",...chocolateBars]
+}
+         var array =[1]
+function destructivelyAddElementToBeginningOfArray(array, foo){
+          array.unshift(foo)
+          return array
+}         
+        
+
+function addElementToEndOfArray(array, foo){
+          return [...array, foo]
+}
+
+function destructivelyAddElementToEndOfArray(array,foo){
+          array.push(foo)
+          return array
+}         
+
+var  array = [1,2,3];
+function accessElementInArray(array, index){
+          return array[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
+          return array.slice(1)
+  
+}
